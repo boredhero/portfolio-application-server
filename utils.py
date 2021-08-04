d573dc7820f1be964b5a24c0fb8dd3a49e647c5e
Noah <personal.boredhero@gmail.com>
2021-08-04 14:49:41 -0400

    Add static providers for config variables

--- utils.py
+++ utils.py
@@ -2,9 +2,25 @@ import json
 import yaml
 import os
 
+class Singleton(type):
+    _instances = {}
+    
+    def __call__(cls, *args, **kwargs):
+        """
+        Make me your metaclass to be a singleton! It's *magic*
+        """
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 class SimpleJsonYmlReader:
 
     def __init__(self, filepath):
+        """
+        A very simple json/yml file reader
+
+        :param str filepath: Filepath
+        """
 
         if filepath is not None:
             self.FILETYPES = [".json", ".yml", ".JSON", ".YML", ".yaml", ".YAML"]
@@ -48,3 +64,38 @@ class SimpleJsonYmlReader:
         else:
             print(f"SimpleJsonYmlReader: read_file: Error: File extension {self.__ext} not a valid extension!")
         return file
+
+class ConfigProvider(metaclass=Singleton):
+
+    def __init__(self):
+        """
+        Holds config.yml data points for easy access. Will not update when values are changed without a server restart.
+        """
+        self.__config_file = SimpleJsonYmlReader("config.yml")
+        self.__config_dict = self.__config_file.read_file()
+        try:
+            self.port = int(self.__config_dict["port"])
+            self.host = str(self.__config_dict["host"])
+            self.debug_mode = bool(self.__config_dict["debug_mode"])
+        except Exception as e:
+            print(f"ConfigProvider: An unknown error occured or a value was missing from your config.yml. Check your config.yml.TEMPLATE file for a correct example\n", e)
+
+class InfoProivder(metaclass=Singleton):
+
+    def __init__(self):
+        """
+        Holds info.yml data points for easy access. Will not update when values are changed without a server restart.
+        """
+        self.__info_file = SimpleJsonYmlReader("info.yml")
+        self.__info_dict = self.__info_file.read_file()
+        try:
+            self.version = str(self.__info_dict["version"])
+            self.nickname = str(self.__info_dict["nickname"])
+            self.author = str(self.__info_dict["author"])
+            self.license = str(self.__info_dict["license"])
+        except Exception as e:
+            print("InfoProvider: An unknown error occured or a value was missing from your info.yml. Check that your info file exists!\n", e)
+            self.version = "Unknown"
+            self.nickname = "Unknown"
+            self.author = "Unknown"
+            self.license = "Unknown"
