ddb4e8758a5fa70098d3d8aab0dabbb5cdec1dfd
Noah <personal.boredhero@gmail.com>
2021-08-04 14:59:30 -0400

    start on flask stuff

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+from flask import Flask, request
+from flask_classful import FlaskView, route
+from utils import *
+
+config = ConfigProvider()
+HOST = config.host
+PORT = config.port
+DEBUG = config.debug_mode
+
+app = Flask(__name__)
 
 if __name__ == '__main__':
     #app.run(debug=False, host=HOST, port=PORT)
