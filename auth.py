b5c7b3d5a69aa12e64fc067a9eebf812902a3a7e
Noah <personal.boredhero@gmail.com>
2021-08-12 18:06:25 -0400

    Finish auth

--- auth.py
+++ auth.py
@@ -2,24 +2,24 @@ from firebase_admin import firestore, credentials
 import firebase_admin
 from utils import ConfigProvider, Singleton
 
-class FirestoreAuthenticator():
+class AuthHolder(metaclass=Singleton):
 
     def __init__(self):
         """
-        Firestore Auth Object.
-
-        NOTE: This class does not currently support more than one firestore database. You need to assign names to them for that and I haven't built that out yet, so please only talk to one database for the time being.
+        Holds API objects as a singleton so that we don't try to create them over and over again (this would cause issues with firestore, for instance.)
         """
-        self.__config = ConfigProvider()
-        self.firestore_auth_obj = self.__make_auth()
-    
-    def __new__(cls):
-            return self.firestore_auth_obj
+        self.firestore = self.__firestore_authenticator()
 
-    def __make_auth(self):
+    def __firestore_authenticator(self):
+        """
+        Create a firestore client object
+
+        :returns: Firestore Client() if successful, else returns None if an Exception occurred
+        """
+        config = ConfigProvider()
         try:
             # Initialize key file
-            key = credentials.Certificate(self.__config.firestore_key_filepath)
+            key = credentials.Certificate(config.firestore_key_filepath)
             # Initialize the app
             firebase_admin.initialize_app(key)
             # Create and return the client object
