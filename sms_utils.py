e9262fa57113fc34fd166c55493d742cd2bd05a8
Noah <personal.boredhero@gmail.com>
2021-08-25 00:16:05 -0400

    start on twilio utils and add config

---
+++ sms_utils.py
@@ -0,0 +1,12 @@
+from auth import AuthHolder
+from utils import ConfigProvider
+
+class TwilioDispatcher():
+
+    def __init__(self):
+        """
+        Twilio dispatch class
+        """
+        
+
+    def dispatch(self, message_body, to_phone, twilio_msg_obj=None)
