04c26ec98f9f8f21d3e63960a4b36a5df1f88936
Noah <personal.boredhero@gmail.com>
2021-08-25 00:26:47 -0400

    Twilio dispatcher

--- sms_utils.py
+++ sms_utils.py
@@ -1,3 +1,4 @@
+import twilio
 from auth import AuthHolder
 from utils import ConfigProvider
 
@@ -8,5 +9,23 @@ class TwilioDispatcher():
         Twilio dispatch class
         """
         self.__conf = ConfigProvider()
+        self.__auth = AuthHolder()
+        self.__tw = self.__auth.twilio
 
-    def dispatch(self, message_body, to_phone, twilio_msg_obj=None)
+    def dispatch(self, message_body, to_phone, twilio_msg_obj=None):
+        if twilio_msg_obj is not None:
+            try:
+                twilio_msg_obj.sid
+            except Exception as e:
+                print("An issue occured trying to send a pre-constructed twilio msg object via Twilio\n", e)
+        else:
+            try:
+                twilio_msg_obj = self.__tw.messages.create(
+                    body = message_body,
+                    from_ = self.__conf.twilio_from_phone,
+                    to = to_phone
+                )
+                twilio_msg_obj.sid
+            except Exception as e:
+                print("An issue occured trying to construct or send an SMS via Twilio.\n", e)
+        
